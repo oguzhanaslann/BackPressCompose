fd2852c3746a3236e4ca8dfbae3c849738bec16b
oguzhanaslann <oguz21199@gmail.com>
2023-01-22 22:52:47 +0300

    - backPressed with activity

--- app/src/main/java/com/example/backpresscompose/MainActivity.kt
+++ app/src/main/java/com/example/backpresscompose/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.backpresscompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.BackHandler
-import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -18,13 +20,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.backpresscompose.ui.theme.BackPressComposeTheme
 import java.util.*
 
-class MainActivity : ComponentActivity() {
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -32,6 +35,16 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 MainScreen()
             }
+        }
+
+        onBackPressedDispatcher.addCallback(
+            callbackGenerator("MainActivity")
+        )
+    }
+
+    private fun callbackGenerator(title: String) = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            Log.e(TAG, "handleOnBackPressed:  $title")
         }
     }
 }
@@ -44,7 +57,12 @@ fun MainScreen() {
     ) {
         val selected = remember { mutableStateOf(0) }
         val selectionStack = remember { Stack<Int>() }
-        BackHandler(selected.value != 0) {
+        val handleBackHandler = remember(selected.value) {
+            selectionStack.isNotEmpty().or(selected.value != 0)
+        }
+
+        BackHandler(handleBackHandler) {
+            Log.e(TAG, "MainScreen: BackHandler ")
             selected.value = if (selectionStack.isEmpty()) 0 else selectionStack.pop()
         }
 
